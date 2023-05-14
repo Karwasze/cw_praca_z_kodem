9a554520cd4ce478261b280f9d528e700a8a3c3b
Karol Kędzierski <karolkedzierski@live.com>
2023-05-14 11:36:44 +0200

    Fixed suppresing lint errors and improved app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+"""Module flask used for creating a web server"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    """Index route for the web app"""
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
 
 
